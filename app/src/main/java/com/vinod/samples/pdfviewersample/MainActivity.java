8ab24505141ae6ff51addaa8d2f992025209c2c4
vinodaw <vinod.awar@kony.com>
2019-06-24 23:36:25 +0700

    initial commit

---
+++ app/src/main/java/com/vinod/samples/pdfviewersample/MainActivity.java
@@ -0,0 +1,48 @@
+package com.vinod.samples.pdfviewersample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PDFView pdfView;
+    private Button btnAsset;
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnAsset = findViewById(R.id.btnAssets);
+        btnAsset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: From Assets");
+                
+            }
+        });
+      /*  Uri uri = Uri.parse("https://www.morningstar.in/mutualfunds/f0000119cb/axis-growth-opportunities-fund-regular-growth/fund-factsheet.aspx");
+        pdfView = findViewById(R.id.pdfView);
+        pdfView.fromUri(uri).enableSwipe(true) // allows to block changing pages using swipe
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
+                .password(null)
+                .scrollHandle(null)
+                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+                // spacing between pages in dp. To define spacing color, set view background
+                .spacing(0)
+                .autoSpacing(false) // add dynamic spacing to fit each page on its own on the screen
+                .pageFitPolicy(FitPolicy.WIDTH)
+                .pageSnap(true) // snap pages to screen boundaries
+                .pageFling(false) // make a fling change only a single page like ViewPager
+                .nightMode(false) // toggle night mode
+                .load();*/
+    }
+}
